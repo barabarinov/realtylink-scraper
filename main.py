af378798dda3454bd718b6572def7f1d459468ac
barabarinov <barabarinov@gmail.com>
2024-05-20 21:32:57 +0300

    Added --output flag, added creation of .json file

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import argparse
+import json
+import logging
+
 from app.apartments_scraper import ApartmentsScraper
-from app.writer import JSONWriter
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Scrape apartment data and save to a JSON file."
+    )
+    parser.add_argument(
+        "--output",
+        nargs="?",
+        type=str,
+        default="apartments.json",
+    )
+    args = parser.parse_args()
+
     scraper = ApartmentsScraper()
     apartments_list = scraper.scrape()
-    print("✅ Scraped successfully")
+    logging.info("✅ Scraped successfully")
 
-    json_writer = JSONWriter("apartments.json")
-    json_writer.write(apartments_list)
+    with open(args.output, 'w', encoding='utf-8') as file:
+        json.dump(apartments_list, file, ensure_ascii=False, indent=4)
